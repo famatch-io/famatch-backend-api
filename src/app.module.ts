61de73744e6e52343763517d6d2a181cf3eb364d
Tom Tang <jjjj1997@live.hk>
2023-05-21 17:29:56 +0800

    feat: setup config var

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
