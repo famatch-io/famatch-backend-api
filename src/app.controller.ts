bc82df6f3e5a531a5fd9c5ee7c5beda759712ecc
Tom Tang <jjjj1997@live.hk>
2023-05-23 00:44:31 +0800

    chore: add test endpoint to show flow working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Notification } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test-notifications')
+  async getNotifications(): Promise<Notification[]> {
+    return this.appService.getTestNotis();
+  }
+
+  @Post('test-notifications')
+  async generateOneNotification(): Promise<Notification> {
+    return this.appService.generateOneTestNoti();
   }
 }
