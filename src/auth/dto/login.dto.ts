9be5c5f7574ad4b8dfdb83385c17de1813ac35a0
Tom Tang <jjjj1997@live.hk>
2023-06-19 22:26:22 +0800

    chore: add swagger description to LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({ description: 'username or email' })
   @IsNotEmpty()
   @IsString()
   username: string;
